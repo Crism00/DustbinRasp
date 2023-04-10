41c1eff65422cc580fa8eb3145c0f21882ae20fb
Crism00 <107272848+Crism00@users.noreply.github.com>
2023-04-10 13:18:07 -0600

    Pa testiar

---
+++ Sensores.py
@@ -0,0 +1,7 @@
+import serial
+
+ser = serial.Serial('/dev/ttyUSB0', 9600) # especifica el puerto y la velocidad de transmisión de datos
+
+while True:
+    data = ser.readline().decode('utf-8').rstrip() # lee los datos que llegan por el puerto serial
+    print(data) # muestra los datos recibidos en la consola
