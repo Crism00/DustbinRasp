60eb6cd9dae7542d4fb22caf5b64c8953557acbb
Crism00 <107272848+Crism00@users.noreply.github.com>
2023-04-13 17:40:32 -0600

    Intento de recibir datos

---
+++ Menu.py
@@ -0,0 +1,6 @@
+import Sensores
+
+Sensor1 = Sensores();
+
+data = Sensor1.recibir();
+Sensor1.jotason(data);
--- Sensores.py
+++ Sensores.py
@@ -1,7 +1,32 @@
 import serial
+import json
+import pymongo
 
-ser = serial.Serial('/dev/ttyACM0', 9600) #especifica el puerto y la velocidad de trasmisión de datos
+class Sensor:
+    def __init__(self ,tipo="sen", id="SEN", valor=0, pinOut=0, pinIn=0):
+        self.tipo = tipo
+        self.id = id
+        self.valor = valor
+        self.pinOut = pinOut
+        
+    def recibir():
+        ser = serial.Serial('/dev/ttyUSB0', 9600) # especifica el puerto y la velocidad de transmisión de datos
 
-while True:
-	data = ser.readline().decode('utf-8').rstrip() #lee los datos que llegan por el puerto serial
-	print(data) #muestra los datos recibidos en la consola
+        while True:
+            data = ser.readline().decode('utf-8').rstrip() # lee los datos que llegan por el puerto serial
+        return data;
+    
+    def jotason(data):
+        datosJson = json.dumps(data) # convierte los datos a formato json
+        print(datosJson)
+        return datosJson;
+        
+    def conexionMongo(datos):
+        cliente = pymongo.MongoClient("mongodb+srv://root:admin@cluster0.urq1yx8.mongodb.net/?retryWrites=true&w=majority")
+        # Selecciona una base de datos
+        db = client["Prueba"]
+        # Selecciona una colección
+        collection = db["Datos"]
+        # Ejemplo de inserción de un documento
+        
+        x = collection.insert_one(datos)
