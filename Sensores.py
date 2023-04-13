ee7a7ff51c8b673ed3ef847312c69d0326140e16
Crism00 <107272848+Crism00@users.noreply.github.com>
2023-04-13 13:20:01 -0600

    Recibir datos

--- Sensores.py
+++ Sensores.py
@@ -1,7 +1,7 @@
 import serial
 
-ser = serial.Serial('/dev/ttyUSB0', 9600) # especifica el puerto y la velocidad de transmisión de datos
+ser = serial.Serial('/dev/ttyACM0', 9600) #especifica el puerto y la velocidad de trasmisión de datos
 
 while True:
-    data = ser.readline().decode('utf-8').rstrip() # lee los datos que llegan por el puerto serial
-    print(data) # muestra los datos recibidos en la consola
+	data = ser.readline().decode('utf-8').rstrip() #lee los datos que llegan por el puerto serial
+	print(data) #muestra los datos recibidos en la consola
