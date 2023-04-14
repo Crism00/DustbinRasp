5d745e98014885b467016e94925f03805320e7ff
Crism00 <107272848+Crism00@users.noreply.github.com>
2023-04-13 22:54:16 -0600

    Chat gpt me corrigio

--- Sensores.py
+++ Sensores.py
@@ -3,28 +3,28 @@ import json
 import pymongo
 
 class Sensor:
-    def __init__(self ,tipo="sen", id="SEN", valor=0, pinOut=0, pinIn=0):
+    def __init__(self, tipo="sen", id="SEN", valor=0, pinOut=0, pinIn=0):
         self.tipo = tipo
         self.id = id
         self.valor = valor
         self.pinOut = pinOut
         
-    def recibir():
+    def recibir(self):
         ser = serial.Serial('/dev/ttyUSB0', 9600) # especifica el puerto y la velocidad de transmisión de datos
 
         while True:
             data = ser.readline().decode('utf-8').rstrip() # lee los datos que llegan por el puerto serial
-        return data;
+            return data
     
-    def jotason(data):
+    def jotason(self, data):
         datosJson = json.dumps(data) # convierte los datos a formato json
         print(datosJson)
-        return datosJson;
+        return datosJson
         
-    def conexionMongo(datos):
+    def conexionMongo(self, datos):
         cliente = pymongo.MongoClient("mongodb+srv://root:admin@cluster0.urq1yx8.mongodb.net/?retryWrites=true&w=majority")
         # Selecciona una base de datos
-        db = client["Prueba"]
+        db = cliente["Prueba"]
         # Selecciona una colección
         collection = db["Datos"]
         # Ejemplo de inserción de un documento
