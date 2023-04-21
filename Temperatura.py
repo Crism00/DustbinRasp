836c64535fce9c0feb805fb4813f2e4d877df692
nadia356 <110473332+nadia356@users.noreply.github.com>
2023-04-20 19:38:58 -0600

    NS

--- Temperatura.py
+++ Temperatura.py
@@ -7,7 +7,7 @@ import json
 import os.path
 
 class DHTSensor( ):
-    def __init__(self, pin, Pin, client):
+    def __init__(self, pin, Pin):
         super().__init__()
         self.dhtDevice = adafruit_dht.DHT11(pin)
         self.pin = Pin
@@ -18,25 +18,14 @@ class DHTSensor( ):
             temperature_c = self.dhtDevice.temperature
             temperature_f = temperature_c * (9 / 5) + 32
             humidity = self.dhtDevice.humidity
+            
+            
             return temperature_c, temperature_f, humidity
         except RuntimeError as error:
             print(error.args[0])
-            raise
+            return None
         
-    def save_to_mongo(self, temperature_c, temperature_f, humidity):
-        db = self.client['sensor_data']
-        temperatura_collection = db['temperatura']
-        temperatura_data = {
-            'tipo': 'Temperatura',
-            'id_sensor': '6',
-            'valor': {
-                'temperatura': temperature_c,
-                'temperatura:': temperature_f, 
-                'humedad': humidity
-            }
-        }
-        temperatura_collection.insert_one(temperatura_data)
-
+  
         
 
     def limpiar(self):
@@ -44,14 +33,31 @@ class DHTSensor( ):
 
     
 if __name__ == "__main__":
-    client = pymongo.MongoClient("mongodb+srv://admin:<password>@cluster0.qf2sgqk.mongodb.net/test")
-    temperatura = DHTSensor(board.D16, 16,client)
+    # Connect to MongoDB
+    client = pymongo.MongoClient("<mongo_uri>")
+    db = client.test_database
+    collection = db.test_collection
+
+    # Create a DHTSensor object
+    temperatura = DHTSensor(board.D16)
+
     while True:
-
         temperature_c, temperature_f, humidity = temperatura.get_temperatures()
-        temperatura.save_to_mongo(temperature_c, temperature_f, humidity)
+        if temperature_c is not None and humidity is not None:
+            # Create a dictionary object to store the data
+            data = {"temperature_c": temperature_c, "temperature_f": temperature_f, "humidity": humidity}
+
+            # Insert the data into the MongoDB collection
+            collection.insert_one(data)
+
+            print("Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(temperature_f, temperature_c, humidity))
+        else:
+            print("Failed to retrieve data from sensor")
+
+        time.sleep(20)
 
 
-        print("Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(temperature_f, temperature_c, humidity))
 
-        time.sleep(20)  # 
+
+
+
