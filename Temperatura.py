4de55656feaee117dfeb327494698bbcf1a9c84e
nadia356 <110473332+nadia356@users.noreply.github.com>
2023-04-20 19:02:29 -0600

    NS

--- Temperatura.py
+++ Temperatura.py
@@ -7,20 +7,38 @@ import json
 import os.path
 
 class DHTSensor( ):
-    def __init__(self, pin, Pin):
+    def __init__(self, pin, Pin,client):
         super().__init__()
         self.dhtDevice = adafruit_dht.DHT11(pin)
         self.pin = Pin
+        self.client = client
 
     def get_temperatures(self):
         try:
             temperature_c = self.dhtDevice.temperature
             temperature_f = temperature_c * (9 / 5) + 32
             humidity = self.dhtDevice.humidity
+            
+            self.save_to_mongo(temperature_c, temperature_f, humidity)
             return print("Temperatura F: {:.1f}, Temperatura C: {:.1f}, Humedad: {}%".format(temperature_f, temperature_c, humidity))
         except RuntimeError as error:
             print(error.args[0])
             return None
+        
+    def save_to_mongo(self, temperature_c, temperature_f, humidity):
+        db = self.client['sensor_data']
+        temperatura_collection = db['temperatura']
+        temperatura_data = {
+            'tipo': 'Temperatura',
+            'id_sensor': '6',
+            'valor': {
+                'temperatura': temperature_c,
+                'temperatura:': temperature_f, 
+                'humedad': humidity
+            }
+        }
+        temperatura_collection.insert_one(temperatura_data)
+
         
 
     def limpiar(self):
@@ -28,7 +46,8 @@ class DHTSensor( ):
 
     
 if __name__ == "__main__":
-    temperatura = DHTSensor(board.D16, 16)
+    client = pymongo.MongoClient("mongodb+srv://admin:<password>@cluster0.qf2sgqk.mongodb.net/test")
+    temperatura = DHTSensor(board.D16, 16,client )
     while True:
         temperatura.get_temperatures()
         time.sleep(30)  # sleep for 5 minutes
