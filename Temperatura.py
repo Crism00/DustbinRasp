09e3dfc91e60818f7d9ad5bbbc77257e51ebcb7a
nadia356 <110473332+nadia356@users.noreply.github.com>
2023-04-20 18:40:52 -0600

    NS

--- Temperatura.py
+++ Temperatura.py
@@ -2,24 +2,28 @@ import time
 import board
 import adafruit_dht
 import pymongo
+import json
+
+import os.path
 
 class DHTSensor( ):
-    def __init__(self, pin, Pin, client):
+    def __init__(self, pin, Pin):
         super().__init__()
         self.dhtDevice = adafruit_dht.DHT11(pin)
         self.pin = Pin
-        self.client = client
 
     def get_temperatures(self):
         try:
             temperature_c = self.dhtDevice.temperature
             temperature_f = temperature_c * (9 / 5) + 32
             humidity = self.dhtDevice.humidity
-            return temperature_c, temperature_f, humidity
+            self.save_to_mongo(temperature_c, temperature_f, humidity)
+            return print("Temperatura F: {:.1f}, Temperatura C: {:.1f}, Humedad: {}%".format(temperature_f, temperature_c, humidity))
+
+
         except RuntimeError as error:
             print(error.args[0])
             return None
-    
     def save_to_mongo(self, temperature_c, temperature_f, humidity):
         db = self.client['sensor_data']
         temperatura_collection = db['temperatura']
@@ -42,14 +46,20 @@ class DHTSensor( ):
             'descripcion': 'Sensor de temperatura y humedad DHT11'
         }
         sensor_collection.insert_one(sensor_data)
+    
+    
+
 
     def limpiar(self):
         self.dhtDevice.exit()
 
+    
 if __name__ == "__main__":
     client = pymongo.MongoClient('mongodb+srv://admin:<password>@cluster0.qf2sgqk.mongodb.net/test')
-    temperatura = DHTSensor(board.D16, 16, client)
+
+    temperatura = DHTSensor(board.D16, 16)
     while True:
-        temperature_c, temperature_f, humidity = temperatura.get_temperatures()
-        temperatura.save_to_mongo(temperature_c, temperature_f, humidity)
-        time.sleep(300)  # sleep for 5 minutes
+        temperatura.get_temperatures()
+        time.sleep(30)  # sleep for 5 minutes
+
+
