9ceac152944347978bc2bd0f96d3d6861af1cb8c
nadia356 <110473332+nadia356@users.noreply.github.com>
2023-04-20 19:10:57 -0600

    NS

--- Temperatura.py
+++ Temperatura.py
@@ -53,7 +53,6 @@ if __name__ == "__main__":
         temperature_c, temperature_f, humidity = temperatura.get_temperatures()
 
 
-        temperatura.save_to_mongo(temperature_c, temperature_f, humidity)
         print("Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(temperature_f, temperature_c, humidity))
 
         time.sleep(30)  # sleep for 5 minutes
