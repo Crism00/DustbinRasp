531fb4ec87eb60c2a83ba4367e38106cf4c790f1
nadia356 <110473332+nadia356@users.noreply.github.com>
2023-04-20 18:27:31 -0600

    ns

--- Temperatura.py
+++ Temperatura.py
@@ -2,15 +2,13 @@ import time
 import board
 import adafruit_dht
 import pymongo
-import json
-
-import os.path
 
 class DHTSensor( ):
-    def __init__(self, pin, Pin):
+    def __init__(self, pin, Pin, client):
         super().__init__()
         self.dhtDevice = adafruit_dht.DHT11(pin)
         self.pin = Pin
+        self.client = client
 
     def get_temperatures(self):
         try:
@@ -21,17 +19,36 @@ class DHTSensor( ):
         except RuntimeError as error:
             print(error.args[0])
             return None
-        
     
-    
+    def save_to_mongo(self, temperature_c, temperature_f, humidity):
+        db = self.client['sensor_data']
+        temperatura_collection = db['temperatura']
+        temperatura_data = {
+            'tipo': 'Temperatura',
+            'id_sensor': '6',
+            'valor': {
+                'temperatura': temperature_c,
+                'humedad': humidity
+            }
+        }
+        temperatura_collection.insert_one(temperatura_data)
 
+        sensor_collection = db['sensor']
+        sensor_data = {
+            'id_sensor': '6',
+            'pin_in': 'D16',
+            'pin_out': 'D16',
+            'descripcion': 'Sensor de temperatura y humedad DHT11'
+        }
+        sensor_collection.insert_one(sensor_data)
 
     def limpiar(self):
         self.dhtDevice.exit()
 
-    
 if __name__ == "__main__":
-    temperatura = DHTSensor(board.D16, 16)
+    client = pymongo.MongoClient('mongodb+srv://admin:<password>@cluster0.qf2sgqk.mongodb.net/test')
+    temperatura = DHTSensor(board.D16, 16, client)
     while True:
-        temperatura.get_temperatures()
-        time.sleep(30)  # sleep for 5 minutes
+        temperature_c, temperature_f, humidity = temperatura.get_temperatures()
+        temperatura.save_to_mongo(temperature_c, temperature_f, humidity)
+        time.sleep(300)  # sleep for 5 minutes
